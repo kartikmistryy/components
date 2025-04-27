6bec30283073460aa5b6c7d418943578ca03caf3
Kartik Mistry <kartikmistry301@gmail.com>
2025-04-27 15:18:19 +0530

    Added scrollTop component

---
+++ scrollTop/src/App.jsx
@@ -0,0 +1,44 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div id='top' className='container'>
+      <a href='#top' className="btn"><img src="./arrow.png" alt="" /></a>
+      <div className="boxParent">
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+      </div>
+    </div>
+  )
+}
+
+export default App
